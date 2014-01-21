ba19ac30dbf265fc2298bebf070c76d7ac71319b
Keith <keithmiketom@gmail.com>
2014-01-21 20:42:14 +0000

    Update accept_form.pl

--- Apache/cgi-bin/accept_form.pl
+++ Apache/cgi-bin/accept_form.pl
@@ -23,7 +23,7 @@ my $address =shift;
 
 my $dbhost='127.0.0.1'; my $dbport=3306;
 my $dsn="DBI:mysql:dbtest;host=$dbhost;port=$dbport";
-$dbh = DBI->connect($dsn, 'dbtestuser', 'dbpassword'
+$dbh = DBI->connect($dsn, 'dbwafuser', 'dbpassword'
                 ) || die "Could not connect to database: $DBI::errstr";
 $sth = $dbh->prepare("insert into custdetails(name,address) values(?,?)");
 $sth->execute($name,$address);
@@ -32,7 +32,7 @@ $sth->execute($name,$address);
 sub showDB {
 my $dbhost='127.0.0.1'; my $dbport=3306;
 my $dsn="DBI:mysql:dbtest;host=$dbhost;port=$dbport";
-$dbh = DBI->connect($dsn, 'dbtestuser', 'dbpassword'
+$dbh = DBI->connect($dsn, 'dbwafuser', 'dbpassword'
                 ) || die "Could not connect to database: $DBI::errstr";
 $sth = $dbh->prepare("select * from custdetails");
 $sth->execute();
